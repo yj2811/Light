e84149ad8a68c4e7a4bbc2371613a189fbf16b0b
yj2811 <yogakshijaiman@gmail.com>
2020-09-01 12:30:03 +0530

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Yogakshi Jaiman on 01/09/20.
+//  Copyright © 2020 Yogakshi Jaiman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
